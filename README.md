e05a1eb5b82b38af5c2554e0f0f8cbff1046edc4
ELIAS  BRODSKY <48387263@ort>
2025-04-25 08:23:33 -0300

    extra terminado

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# TP-5-Primer-servidor-con-Node-y-Express" 
+
+/ → responde con un mensaje de bienvenida (texto plano).
+
+/hora → responde con la hora actual en texto plano.
+
+/fecha-completa → devolve la fecha y hora completa, por ejemplo:
+"sábado 5 de abril de 2025, 16:24:51"
--- app.js
+++ app.js
@@ -14,4 +14,10 @@ app.get('/fecha-completa', (req, res) => {
     res.send(horaCompleta());
 });
 
+app.use((req, res) => {
+    const errorCode = 404;
+    const httpCatUrl = `https://http.cat/${errorCode}`;
+    res.status(errorCode).send(`<img src="${httpCatUrl}" alt="Error 404 - Not Found">`);
+});
+
 export default app;
